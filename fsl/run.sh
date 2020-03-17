24f0854649679a68858d535c1b82d3a2e01f06ea
Aditya Bharti <adibhar97@gmail.com>
2020-03-17 13:44:49 +0530

    More experiments

--- fsl/run.sh
+++ fsl/run.sh
@@ -2,17 +2,21 @@
 #SBATCH -A aditya.bharti
 #SBATCH --cpus-per-task=12
 #SBATCH --gpus=4
-#SBATCH --time=2-00:00:00
+#SBATCH --time=12:00:00
+#SBATCH --mail-user=aditya.bharti@research.iiit.ac.in
+#SBATCH --mail-type=ALL
 
-EXP_NAME="exp13";
+EXP_NAME="exp14";
+NUM_EPOCHS=100;
 
 source "/home/aditya.bharti/python_env/bin/activate";
 pushd "/home/aditya.bharti/FewShot/fsl";
 # commands
 python -m torch.distributed.launch --nproc_per_node=4 main.py \
     --complex_opt \
-    --num_epochs=200 --batch_size=2048 \
+    --first_augment="CropResize" --second_augment="GaussBlur" \
+    --num_epochs=$NUM_EPOCHS --batch_size=2048 \
     --nesterov --momentum=5e-2 --weight_decay=1e-6 \
-    --T_max=200 \
+    --T_max=$NUM_EPOCHS \
     --save_path="saves/${EXP_NAME}.pth" |& tee "saves/${EXP_NAME}.out";
 popd;
